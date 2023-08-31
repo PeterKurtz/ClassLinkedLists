b09971aa336aeb1db427118bddbc2241843d2dad
Peter Kurtz <peterjohnkurtz@gmail.com>
2023-08-30 19:36:21 -0600

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,62 @@
+public class LinkedList <E extends Comparable>{
+    private class ListNode<E> {
+        public E value;
+        public ListNode<E> next;
+
+        public ListNode() {
+        }
+
+        public ListNode(E o) {
+            this.value = o;
+        }
+    }
+    private ListNode<E> head = new ListNode<>();
+    private ListNode<E> tail;
+    private int size;
+
+    public LinkedList() {
+        this.size = 0;
+        tail = head;
+    }
+
+    public int getSize() {return this.size;}
+    public boolean isEmpty() {return this.size == 0;}
+
+    public void insert(E o) {
+        ListNode<E> node = new ListNode<>(o);
+        ListNode<E> current = head.next;
+        ListNode<E> previous = head;
+
+        while(current != null && current.value.compareTo(o)< 0) {
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = node;
+        node.next = current;
+        if (current == null) {
+            tail = node;
+        }
+
+        this.size++;
+    }
+
+    public void remove(E o){
+        ListNode<E> node = head.next;
+        ListNode<E> previous = head;
+
+        while (node.value.compareTo(o) != 0) {
+            previous = node;
+            node = node.next;
+        }
+
+        previous.next = node.next;
+        if (previous.next == null) {
+            tail = previous;
+        }
+        this.size--;
+    }
+
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linked = new LinkedList();
+        linked.insert(0);
+        linked.insert(1);
+        linked.insert(2);
+        linked.insert(3);
+        linked.remove(3);
+    }
+}
